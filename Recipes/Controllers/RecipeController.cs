57598442e67b15045fabe21b27becf9e04c2e7b9
Moses Wynn <accounts@moseswynn.com>
2018-06-19 13:24:55 -0400

    Fix problems with detail action method and view and add comments.

--- Recipes/Controllers/RecipeController.cs
+++ Recipes/Controllers/RecipeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Recipes.Models;
 using Recipes.Data;
 using System.Net;
+using System.Data.Entity;
 
 namespace Recipes.Controllers
 {
@@ -16,23 +17,32 @@ namespace Recipes.Controllers
         // Get list of recipes and display when the home page is opened.
         public ActionResult Index()
         {
-            var Recipes = db.Recipes.ToList();
-            return View(Recipes);
+            var Recipes = db.Recipes.ToList(); //convert the recipes to a list
+            return View(Recipes); //pass the list of recipes to the view
         }
 
-        //Get details of recipe or return 404
+        //Get details of the recipe based on it's id
         public ActionResult Detail(int? id)
         {
+            //if an id was not specified return 400 
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Recipe recipe = db.Recipes.Find(id);
+
+            //find the recipe, including it's ingredients and instructions, based on the specified id.
+            var recipe = db.Recipes
+                .Include(r => r.Ingredients)
+                .Include(r => r.Instructions)
+                .ToList()
+                .Find(r => r.RecipeId == id);
+
             if(recipe == null)
             {
-                return HttpNotFound();
+                return HttpNotFound(); //return 404 if a recipe with the specified id can't be found
             }
-            return View(recipe);
+
+            return View(recipe); //pass the recipe to the view
         }
     }
 }
