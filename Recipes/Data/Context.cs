150367be13f941a0c607a67e46db35dd08ce75db
Moses Wynn <accounts@moseswynn.com>
2018-06-20 16:54:17 -0400

    Removed unused using directives

--- Recipes/Data/Context.cs
+++ Recipes/Data/Context.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Recipes.Models;
+﻿using Recipes.Models;
 using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace Recipes.Data
 {
--- Recipes/Data/DatabaseInitializer.cs
+++ Recipes/Data/DatabaseInitializer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
 using Recipes.Models;
 namespace Recipes.Data
 {
