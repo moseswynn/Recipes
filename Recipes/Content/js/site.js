e2a6f1921382749b7d7ba21a0097c244be7f61bd
Moses Wynn <accounts@moseswynn.com>
2018-06-20 12:05:13 -0400

    Wrote javascript to package form data into payload and post to server with fetch.

--- Recipes/Content/js/site.js
+++ Recipes/Content/js/site.js
@@ -24,16 +24,59 @@ function removeFromList(thing) {
     }
 }
 
-function submitRecipeForm(ParameterTitle, RecipeId) {
-    //TODO: make a list of strings out of the ingredients
+function childrenValuesToArray(element) {
+    //returns the values of the children of an element as an array
+    var elementChildren = [].slice.call(element.children);
+    var values = []
+    elementChildren.forEach(function (element) { values.push(element.value) });
+    return values
+}
 
-    //TODO: make a list of strings out of the instructions
+function submitRecipeForm(ParameterTitle, RecipeId) { 
+    /*This exists because using MVC forms would not work due
+     * to the number of inputs not being constant as the number
+     * of ingredients and instruction steps vary.*/
 
-    //TODO: make an object of all the form data
 
-    //TODO: if the ParameterTitle is 'edit' then bundle the response object with the RecipeId and send AJAX to 'Edit' ActionMethod
-
-    //TODO: if the Parametertitle is 'create' then bundle the form data as a response object and send AJAX to 'New' ActionMethod
-
+    //package form data into an object:
+    //1) capture name and description
+    var name = $('#nameTextBox')[0].value;
+    var description = $('#descriptionTextArea')[0].value
+    //makes an array of objects from the ingredients and instructions
+    var ingredientStrings = childrenValuesToArray($('#ingredients')[0]);
+    var instructionStrings = childrenValuesToArray($('#instructions')[0]);
+    var ingredients = [];
+    for (i = 0; i < ingredientStrings.length; i++) {
+        ingredients.push({ 'ingredientText': ingredientStrings[i] });
+    }
+    var instructions = [];
+    for (i = 0; i < instructionStrings.length; i++) {
+        instructions.push({
+            'instructionIndex': i,
+            'instructionText': instructionStrings[i]
+        });
+    }
+    //package all form data into payload
+    var payLoad = {
+        'Name': name,
+        'Description': description,
+        'Instructions': instructions,
+        'Ingredients': ingredients
+    };
+    //add RecipeId to the payload if the ParameterTitle is edit
+    if (ParameterTitle == 'edit') {
+        payLoad['RecipeId'] = RecipeId
+    }
+    //TODO: POST data and wait for response, if success redirect to home, else post error
+    fetch('~/Recipe/Save', {
+        method: 'POST',
+        body: JSON.stringify(payLoad),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .catch(error => console.error('error:', error))
+        .then(response => console.log('success:', response));
     //TODO: check if response is success, if yes redirect to list view, else display error message
 }
